2010e67726e083240911f492e6ac1bfd0e917a1c
Mathi Fonseca <mathifonseca@gmail.com>
2014-11-13 17:59:02 -0200

    updated various dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,41 +15,41 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 		
-		compile 'de.javakaffee.msm:memcached-session-manager:1.6.4', {
+		compile 'de.javakaffee.msm:memcached-session-manager:1.8.3', {
 			excludes 'CouchbaseMock', 'annotations', 'catalina', 'catalina-ha', 'couchbase-client', 'coyote',
 			         'hibernate-annotations', 'hibernate-core', 'hsqldb', 'httpclient', 'javassist',
 			         'jettison', 'jmemcached-core', 'jsr305', 'mockito-core', 'netty', 'servlet-api',
 			         'slf4j-simple', 'spymemcached', 'testng'
 		}
 
-		compile 'de.javakaffee.msm:memcached-session-manager-tc7:1.6.4', {
+		compile 'de.javakaffee.msm:memcached-session-manager-tc7:1.8.3', {
 			excludes 'CouchbaseMock', 'annotations', 'hibernate-annotations', 'hibernate-core', 'hsqldb',
 			         'httpclient', 'javassist', 'jettison', 'jmemcached-core', 'jsr305', 'memcached-session-manager',
 			         'mockito-core', 'netty', 'slf4j-simple', 'testng', 'tomcat-catalina', 'tomcat-catalina-ha',
 			         'tomcat-coyote', 'tomcat-servlet-api'
 		}
 
-		runtime 'spy:spymemcached:2.8.12', {
+		runtime 'net.spy:spymemcached:2.11.4', {
 			excludes 'log4j', 'spring-beans'
 		}
 
-		runtime 'couchbase:couchbase-client:1.1.6', {
+		runtime 'com.couchbase.client:couchbase-client:1.4.5', {
 			excludes 'commons-codec', 'httpcore', 'httpcore-nio', 'jettison', 'netty', 'spymemcached'
 		}
 
-		runtime 'org.codehaus.jettison:jettison:1.3.3', {
+		runtime 'org.codehaus.jettison:jettison:1.3.6', {
 			excludes 'junit', 'stax-api', 'wstx-asl'
 		}
 
-		runtime 'org.apache.httpcomponents:httpcore:4.2.4', {
+		runtime 'org.apache.httpcomponents:httpcore:4.3.3', {
 			excludes 'junit', 'mockito-core'
 		}
 
-		runtime 'org.apache.httpcomponents:httpcore-nio:4.2.4', {
+		runtime 'org.apache.httpcomponents:httpcore-nio:4.3.3', {
 			excludes 'commons-logging', 'httpcore', 'junit', 'mockito-core'
 		}
 
-		runtime 'io.netty:netty:3.6.5.Final', {
+		runtime 'io.netty:netty:3.9.5.Final', {
 			excludes 'activation', 'commons-logging', 'easymock', 'easymockclassextension', 'jboss-logging-spi',
 			         'jboss-marshalling', 'jboss-marshalling-river', 'jboss-marshalling-serial', 'junit', 'log4j',
 			         'org.osgi.compendium', 'org.osgi.core', 'protobuf-java', 'servlet-api', 'slf4j-api', 'slf4j-simple'
@@ -57,7 +57,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build(':release:2.2.1', ':rest-client-builder:1.0.3') {
+		build(':release:3.0.1', ':rest-client-builder:2.0.3') {
 			export = false
 		}
 	}
