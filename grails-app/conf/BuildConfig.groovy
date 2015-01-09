46384824815ea8e82e782ee01c48e4e41986a8b8
Mathi Fonseca <mathifonseca@gmail.com>
2015-01-09 17:15:36 -0200

    more updates for Tomcat 8 compatibility

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,77 +2,59 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+grails.project.repos.peya.url = "http://54.209.219.49:8081/nexus/content/repositories/thirdparty/"
+grails.project.repos.peya.type = "maven"
+grails.project.repos.peya.username = "deployment"
+grails.project.repos.peya.password = "peya"
+grails.project.repos.default = "peya"
 
-    // configure settings for the test-app JVM, uses the daemon by default
+grails.project.fork = [
     test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
     run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
     war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
-grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
     inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn"
     repositories {
         grailsCentral()
         mavenLocal()
         mavenCentral()
 		mavenRepo 'http://files.couchbase.com/maven2/'
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-		compile 'org.apache.tomcat:tomcat-catalina:8.0.15'
-		
-        compile 'de.javakaffee.msm:memcached-session-manager:1.8.3', {
+        compile 'de.javakaffee.msm:memcached-session-manager-tc8:1.8.3', {
 			excludes 'CouchbaseMock', 'annotations', 'catalina', 'catalina-ha', 'couchbase-client', 'coyote',
 			         'hibernate-annotations', 'hibernate-core', 'hsqldb', 'httpclient', 'javassist',
 			         'jettison', 'jmemcached-core', 'jsr305', 'mockito-core', 'netty', 'servlet-api',
 			         'slf4j-simple', 'spymemcached', 'testng'
 		}
 
-		compile 'de.javakaffee.msm:memcached-session-manager-tc7:1.8.3', {
-			excludes 'CouchbaseMock', 'annotations', 'hibernate-annotations', 'hibernate-core', 'hsqldb',
-			         'httpclient', 'javassist', 'jettison', 'jmemcached-core', 'jsr305', 'memcached-session-manager',
-			         'mockito-core', 'netty', 'slf4j-simple', 'testng', 'tomcat-catalina','tomcat-catalina-ha',
-			         'tomcat-coyote', 'tomcat-servlet-api'
-		}
-
-		runtime 'net.spy:spymemcached:2.11.4', {
+		runtime 'net.spy:spymemcached:2.11.5', {
 			excludes 'log4j', 'spring-beans'
 		}
 
-		runtime 'com.couchbase.client:couchbase-client:1.4.5', {
+		runtime 'com.couchbase.client:couchbase-client:1.4.6', {
 			excludes 'commons-codec', 'httpcore', 'httpcore-nio', 'jettison', 'netty', 'spymemcached'
 		}
 
-		runtime 'org.codehaus.jettison:jettison:1.3.6', {
+		runtime 'org.codehaus.jettison:jettison:1.3.7', {
 			excludes 'junit', 'stax-api', 'wstx-asl'
 		}
 
-		runtime 'org.apache.httpcomponents:httpcore:4.3.3', {
+		runtime 'org.apache.httpcomponents:httpcore:4.4', {
 			excludes 'junit', 'mockito-core'
 		}
 
-		runtime 'org.apache.httpcomponents:httpcore-nio:4.3.3', {
+		runtime 'org.apache.httpcomponents:httpcore-nio:4.4', {
 			excludes 'commons-logging', 'httpcore', 'junit', 'mockito-core'
 		}
 
-		runtime 'io.netty:netty:3.9.5.Final', {
+		runtime 'io.netty:netty-all:4.0.25.Final', {
 			excludes 'activation', 'commons-logging', 'easymock', 'easymockclassextension', 'jboss-logging-spi',
 			         'jboss-marshalling', 'jboss-marshalling-river', 'jboss-marshalling-serial', 'junit', 'log4j',
 			         'org.osgi.compendium', 'org.osgi.core', 'protobuf-java', 'servlet-api', 'slf4j-api', 'slf4j-simple'
